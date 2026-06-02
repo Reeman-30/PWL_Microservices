11946e75bdb05d863bf2ec97ba5fdb2431f7b486
Reeman-30 <rayyanpratama30@gmail.com>
2026-06-02 19:06:37 +0700

    Praktikum 7

---
+++ client-apps/src/components/microservices/user-services/index.tsx
@@ -0,0 +1,13 @@
+import { Alert } from "@/components/ui/alerts";
+import React from "react";
+
+export default function UserManagement() {
+  return (
+    <div>
+      <Alert
+        message="Please proceed with creating the User Management functionality using the RESTful API from the previously developed module."
+        variant="info"
+      />
+    </div>
+  );
+}
